e0c16fa012052c4306d9f08e37143873f820dc0b
MenaceG <ayushg72963@gmail.com>
2025-10-23 00:04:45 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from './navbar';
 import './output.css';
-import IconCloud from './iconcloud';
-
+import IconCloud from './components/iconcloud';
 
 import carnaamalogo from "./logos/carnaama.jpg"
 import hexovisionlogo from "./logos/hexovision_logo.jpg"
@@ -10,17 +9,17 @@ import mindCheck from "./projects/mindcheck.png"
 
 function App() {
   return (
-    <div className="App bg-black text-white flex items-center flex-col">
+    <div className="App bg-black text-white flex items-center flex-col w-100%">
       <Navbar />
 
-      <div className="intro mb-10 min-w-200">
-        <h2 className="font-bold text-3xl ">Intro</h2>
+      <div className="intro m-2 mb-10 sm:w-165">
+        <h2 className="font-bold text-3xl">Intro</h2>
         <h1 className='dark:text-white/70'>Hi, i'm Ayush </h1>
         <p className='dark:text-white/70'>I am a third year CS student who loves building apps and websites that are useful</p>
         <p className='dark:text-white/70'>I am very active on x/twitter where i share my learning journey and make new friends.</p>
       </div>
 
-      <div className='about mb-10 max-w-200'>
+      <div className='about m-2 mb-10 sm:w-165'>
         <h2 className="font-bold text-3xl">About</h2>
         <p className='dark:text-white/70'>I have been deeply interested in computers since childhood, spending countless hours exploring and
           experimenting with technology. This passion led me to pursue a Bachelor of Computer Applications
@@ -29,11 +28,11 @@ function App() {
           development stacks, driven by a desire to improve and expand my skillset.</p>
       </div>
 
-      <div className="skills mb-10 min-w-200">
-        <h2 className="w-full md:w-full font-bold text-3xl mb-4">Skills</h2>
-        <div className='flex border-2 border-[#1c1c1e] rounded-lg p-4'>
-          <div className="w-half skillBox m-4 mr-20">
-            <ul className="grid grid-cols-3 gap-4">
+      <div className="skills m-2 mb-10 sm:w-165">
+        <h2 className="w-full font-bold text-3xl mb-4">Skills</h2>
+        <div className='flex flex-col border-2 border-[#1c1c1e] rounded-lg p-4'>
+          <div className="w-half skillBox">
+            <ul className="grid grid-cols-3 gap-4 content-center ">
               <li className="px-4 py-2 bg-gray-800 text-blue-400 rounded-full">React</li>
               <li className="px-4 py-2 bg-gray-800 text-green-400 rounded-full">PostgreSQL</li>
               <li className="px-4 py-2 bg-gray-800 text-green-300 rounded-full">MongoDB</li>
@@ -52,7 +51,7 @@ function App() {
               <li className="px-4 py-2 bg-gray-800 text-purple-400 rounded-full">Solidity</li>
             </ul>
           </div>
-          <div className='flex w-half skillsphere'>
+          <div className='flex w-half justify-center skillsphere'>
             <IconCloud
               iconSlugs={["react", "postgresql", "mongodb", "javascript", "mysql", "flask", "python", "java", "c", "cplusplus", "nodedotjs", "expressjs", "tailwindcss", "git", "html", "css", "github", "linux", "stackoverflow", "visualstudiocode", "android", "androidstudio", "vercel  ", "vim", "angular", "aws", "docker", "npm",]}
             />
@@ -62,7 +61,7 @@ function App() {
       </div>
 
 
-      <div className='work mb-10 min-w-200'>
+      <div className='work m-2 mb-10 w-105 sm:w-165'>
         <h2 className="font-bold text-3xl mb-4">Work</h2>
         <div className='IBM border-2 border-[#1c1c1e] rounded-lg mb-2 p-4'>
           <img src={ibmlogo} className="w-12"></img>
@@ -80,17 +79,17 @@ function App() {
           <p className='dark:text-white/70'>Web Developer | Aug2024 - Oct2024 | On-site</p>
 
         </div>
-        
+
       </div>
 
-      <div className='project mb-10 min-w-200'>
+      <div className='project m-2 mb-10 sm:w-165'>
         <h2 className="font-bold text-3xl mb-4">Projects</h2>
-        <div className="projectList flex w-200">
+        <div className="projectList flex">
 
-          <div className="mindCheck border-2 border-[#1c1c1e] rounded-lg w-100 m-2">
-            <img src={mindCheck} className="w-100"></img>
+          <div className="mindCheck border-2 border-[#1c1c1e] rounded-lg m-2">
+            <img src={mindCheck} className=""></img>
             <p className='dark:text-white/70'>MindCheck</p>
-            <ul className="grid grid-cols-4 gap-1 text-xs w-90">
+            <ul className="grid grid-cols-4 gap-1 text-xs ">
               <li className="px-4 py-1 bg-gray-800 rounded-full">React</li>
               <li className="px-4 py-1 bg-gray-800 rounded-full">PostgreSQL</li>
               <li className="px-4 py-1 bg-gray-800 rounded-full">MongoDB</li>
@@ -101,10 +100,10 @@ function App() {
             </ul>
           </div>
 
-          <div className="mindCheck border-2 border-[#1c1c1e] rounded-lg w-100 m-2">
-            <img src={mindCheck} className="w-100"></img>
+          <div className="mindCheck border-2 border-[#1c1c1e] rounded-lg m-2">
+            <img src={mindCheck} className=""></img>
             <p className='dark:text-white/70'>MindCheck</p>
-            <ul className="grid grid-cols-4 gap-1 text-xs w-90">
+            <ul className="grid grid-cols-4 gap-1 text-xs">
               <li className="px-4 py-1 bg-gray-800 rounded-full">React</li>
               <li className="px-4 py-1 bg-gray-800 rounded-full">PostgreSQL</li>
               <li className="px-4 py-1 bg-gray-800 rounded-full">MongoDB</li>
