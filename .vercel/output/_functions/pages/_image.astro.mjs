fb6687e6c881c353ba2e1defa95f36d99d7d449a
MenaceG <ayushg72963@gmail.com>
2025-10-24 00:27:37 +0530

    Update

---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_ByiMpPHI.mjs';
+export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
+
+
+
+
