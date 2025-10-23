14c8aaedcd5ca0d15419a64a50d3da06bd256c43
MenaceG <ayushg72963@gmail.com>
2025-10-24 00:15:43 +0530

    Complete Change

---
+++ src/config.ts
@@ -0,0 +1,85 @@
+export const siteConfig = {
+  name: "Ayush Gupta",
+  title: "Software Engineer",
+  description: "Portfolio website of Ayush Gupta",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "ayush.me1204@gmail.com",
+    linkedin: "https://linkedin.com/in/ayushguptadev",
+    twitter: "https://x.com/MenaceAyush",
+    github: "https://github.com/MenaceG",
+  },
+  aboutMe:
+    "I have been deeply interested in computers since childhood, spending countless hours exploring and experimenting with technology. This passion led me to pursue a Bachelor of Computer Applications (BCA), where I honed my technical skills. I specialize in troubleshooting and fixing code and systems, and I take pride in solving complex problems. I continuously explore new technologies and development stacks, driven by a desire to improve and expand my skillset.",
+  skills: ["react", "postgresql", "mongodb", "javascript", "mysql", "flask", "python", "java", "c", "cplusplus", "nodedotjs", "expressjs", "tailwindcss", "git", "html", "css", "github", "linux", "stackoverflow", "visualstudiocode", "android", "androidstudio", "vercel  ", "vim", "angular", "aws", "docker", "npm"],
+  projects: [
+    {
+      name: "MindCheck",
+      description:
+        "A full-stack web app built with React, Node.js, and MongoDB that provides mental health self-assessments, AI-powered chat support, and nearby doctor recommendations.",
+      link: "https://aidevroundup.com/?ref=devportfolio",
+      skills: ["React", "Node.js", "PostgreSQL", "MongoDB", "ExpressJs", "OpenAI API", "Javascript"],
+    },
+    {
+      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+      description:
+        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+      link: "https://fullstackextensions.com/?ref=devportfolio",
+      skills: ["React", "Node.js", "AWS"],
+    },
+  ],
+  experience: [
+    {
+      company: "IBM",
+      title: "Project Lead",
+      dateRange: "Jun 2025 - Aug 2025",
+      bullets: ["Developed a full-stack MERN web application for mental health assessments.", "Implemented tests for Depression, Anxiety, ADHD, and OCD with structured scoring and insights.","Built features including user profiles, doctor dashboards, Google Maps integration, and printable reports.", "Integrated AI support to provide empathetic, real-time guidance and enhance user experience."
+      ],
+    },
+    {
+      company: "Carnama",
+      title: "Frontend Developer",
+      dateRange: "Oct 2024 - Jan 2025",
+      bullets: ["Designed and developed the user interface for Carnaama's web platform, focusing on creating a modern, intuitive, and visually appealing design." ,"Leveraged ReactJs hooks (useState, useEffect) for state management and optimized rendering, ensuring efficient and maintainable code." ,"Utilized Tailwind CSS to build responsive and highly customizable UI components, ensuring fast development and consistent styling across pages."],
+    },
+    {
+      company: "Hexovision",
+      title: "Web Developer",
+      dateRange: "Aug 2024 - Oct 2024",
+      bullets: ["Developed and launched the company’s website using HTML, CSS, JavaScript, and PHP, ensuring a responsive and user-friendly experience across all devices." ,"Integrated backend services with PHP to enable dynamic content management and real-time updates." ,"Optimized website performance, improving page load speed by 25% through code and asset optimization."
+],
+    },
+  ],
+  education: [
+    {
+      school: "UPES",
+      degree: "Bachelor of Computer Applications",
+      dateRange: "2023 - 2026",
+      achievements: [
+        "Graduated Magna Cum Laude with 3.8 GPA",
+        "Dean's List all semesters",
+        "President of Computer Science Club",
+      ],
+    },
+    {
+      school: "The Odin Project",
+      degree: "Web Development",
+      dateRange: "2023",
+      achievements: [
+        "Completed 500+ hours of coursework",
+        "Built 10+ portfolio projects",
+        "Specialized in React and Node.js",
+      ],
+    },
+    {
+      school: "Ryan International School, Navi Mumbai",
+      degree: "12th",
+      dateRange: "2023",
+      achievements: [
+        "Completed 500+ hours of coursework",
+        "Built 10+ portfolio projects",
+        "Specialized in React and Node.js",
+      ],
+    },
+  ],
+};
