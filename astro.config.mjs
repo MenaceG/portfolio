7fb3873454ed163eed31ebe80722337d034f904d
MenaceG <ayushg72963@gmail.com>
2025-10-24 00:30:35 +0530

    New Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
-    maxDuration: 8,
   }),
   vite: {
     plugins: [tailwindcss()],
