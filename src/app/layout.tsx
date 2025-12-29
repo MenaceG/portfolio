ec93407c64c7b7c2402c4a270092547b703f4753
MenaceG <ayushg72963@gmail.com>
2025-12-29 14:17:12 +0530

    Added Umami Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="9f0f33b7-9f53-413b-954a-328d69d19db2"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
